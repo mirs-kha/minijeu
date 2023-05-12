458ba2438cc7744a24e3fe55dcb474ed6dde5e28
mirs-kha <130989395+mirs-kha@users.noreply.github.com>
2023-05-12 19:46:48 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,108 +1,92 @@
-var score = 0;
-var gagnant = "";
-var player = "";
-var round = 0;
-const choosePersCat = document.querySelector("#cat");
-var cat = "cat";
-var mouse = "mouse";
-var dog = "dog";
-const choosePersMouse = document.querySelector("#mouse");
-const choosePersDog = document.querySelector("#dog");
-const playercard = document.querySelector("#choixjoueur");
-const anime = document.querySelector("#robot");
-const close = document.querySelector(".jouer");
+var score = 0
+var gagnant = ""
+var player = ""
+var round = 0
+const choosePersCat = document.querySelector("#cat")
+var cat = "cat"
+var mouse = "mouse"
+var dog = "dog"
+const choosePersMouse = document.querySelector("#mouse")
+const choosePersDog = document.querySelector("#dog")
+const playercard = document.querySelector("#choixjoueur")
+const anime = document.querySelector("#robot")
+const close = document.querySelector(".jouer")
 const cardsChoise = document.querySelector(".div1")
 
 close.addEventListener("click", function () {
-    document.querySelector(".modal").style.display = "none";
+    document.querySelector(".modal").style.display = "none"
 });
 
 choosePersCat.addEventListener("click", function () {
     if (round <= 2) {
         player = cat;
-        playercard.innerHTML = '<img src="feuilleplay.png">';
-        playercard.setAttribute("class", "div9 choixjoueursortie2");
-        anime.setAttribute("class", "div9 choixjoueursortie");
-
+        playercard.innerHTML = '<img src="../minijeu/assets/img/feuilleplay.png">'
+        playercard.setAttribute("class", "div9 choixjoueursortie2")
+        anime.setAttribute("class", "div9 choixjoueursortie")
         cardsChoise.setAttribute("class", "div1 outCard");
         cardsChoise.setAttribute("display", "none");
-        
-
-
         roundcounter()
     }
     else {
-        finPartie();
-
+        finPartie()
     }
-});
+})
 choosePersMouse.addEventListener("click", function () {
     if (round <= 2) {
-        player = mouse;
-        playercard.innerHTML = '<img src="ciseauxplay.png">';
-        playercard.setAttribute("class", "div9 choixjoueursortie2");
-        anime.setAttribute("class", "div9 choixjoueursortie");
-        cardsChoise.setAttribute("display", "none");
-
-        cardsChoise.setAttribute("class", "div1 outCard");
-
+        player = mouse
+        playercard.innerHTML = '<img src="../minijeu/assets/img/ciseauxplay.png">'
+        playercard.setAttribute("class", "div9 choixjoueursortie2")
+        anime.setAttribute("class", "div9 choixjoueursortie")
+        cardsChoise.setAttribute("display", "none")
+        cardsChoise.setAttribute("class", "div1 outCard")
         roundcounter()
     }
     else {
         finPartie();
-
     }
-});
+})
 choosePersDog.addEventListener("click", function () {
     if (round <= 2) {
-        player = dog;
-        playercard.innerHTML = '<img src="pierreplay.png">';
-        playercard.setAttribute("class", "div9 choixjoueursortie2");
-        anime.setAttribute("class", "div9 choixjoueursortie");
-        cardsChoise.setAttribute("class", "div1 outCard");
-        cardsChoise.setAttribute("display", "none");
-
+        player = dog
+        playercard.innerHTML = '<img src="../minijeu/assets/img/pierreplay.png">'
+        playercard.setAttribute("class", "div9 choixjoueursortie2")
+        anime.setAttribute("class", "div9 choixjoueursortie")
+        cardsChoise.setAttribute("class", "div1 outCard")
+        cardsChoise.setAttribute("display", "none")
         roundcounter()
     }
     else {
-        finPartie();
-
+        finPartie()
     }
-});
-
-
-
+})
 
 function roundcounter() {
     if (round <4) {
         computerRandom()
     } else {
-        finPartie();
-
+        finPartie()
     }
 }
 function computerRandom() {
-    const nul = document.querySelector("#resultat");
-    const win = document.querySelector("#resultat");
-    const monscore = document.querySelector("#score");
-    const loose = document.querySelector("#resultat");
-    const roundAffiche = document.querySelector(".round");
+    const nul = document.querySelector("#resultat")
+    const win = document.querySelector("#resultat")
+    const monscore = document.querySelector("#score")
+    const loose = document.querySelector("#resultat")
+    const roundAffiche = document.querySelector(".round")
 
-    round = round + 1;
-    roundAffiche.innerHTML = round;            
-    var animaux = ["dog", "cat", "mouse"];
+    round = round + 1
+    roundAffiche.innerHTML = round      
+    var animaux = ["dog", "cat", "mouse"]
     animaux.forEach((element) => {
-        animaux.push(element);
-    });
-    var values = animaux;
-    var valueToUse = values[Math.floor(Math.random() * values.length)];
-    var pos = animaux.indexOf(valueToUse);
-
+        animaux.push(element)
+    })
+    var values = animaux
+    var valueToUse = values[Math.floor(Math.random() * values.length)]
+    var pos = animaux.indexOf(valueToUse)
 
     if (animaux[pos] == player) {
+        nul.innerHTML = "match nul"
 
-        nul.innerHTML = "match nul";
-        console.log("entrer");
     } else if (
 
         (player == "dog" && animaux[pos] == "mouse") ||
@@ -110,10 +94,10 @@ function computerRandom() {
         (player == "mouse" && animaux[pos] == "cat")
     ) {
 
-        win.innerHTML = "win";
-        score = score + 1;
-        monscore.innerHTML = score;
-        console.log("entrer");
+        win.innerHTML = "Gagné"
+        score = score + 1
+        monscore.innerHTML = score
+
     } else if (
 
         (animaux[pos] == "dog" && player == "mouse") ||
@@ -121,64 +105,46 @@ function computerRandom() {
         (animaux[pos] == "mouse" && player == "cat")
     ) {
 
-        loose.innerHTML = "lose";
-        console.log("you lose");
+        loose.innerHTML = "Perdu"
 
     }
-    animaux[0] = '<img src="pierreplay.png">';
-    animaux[1] = '<img src="feuilleplay.png">';
-    animaux[2] = '<img src="ciseauxplay.png">';
-    anime.innerHTML = animaux[pos]; 
-    console.log(animaux[pos]);
+    animaux[0] = '<img src="../minijeu/assets/img/pierreplay.png">'
+    animaux[1] = '<img src="../minijeu/assets/img/feuilleplay.png">'
+    animaux[2] = '<img src="../minijeu/assets/img/ciseauxplay.png">'
+    anime.innerHTML = animaux[pos];
 
-    setTimeout(attendreAnime, 5000);
+
+    setTimeout(attendreAnime, 5000)
 }
 
 function finPartie() {
-    console.log("entrerzzzzzzzzzzzzzzzzzzz");
-    const looser = `<img src="dommagehaut.png">
-    
+
+    const looser = `<img src="../minijeu/assets/img/dommagehaut.png">
     <p class="dommage">Dommage !</p>
     <button type="button" value = "Rafraîchir" onclick="history.go(0)" class="jouer" style="margin-bottom: 144px;">REJOUER?</button>
-    <img class="dommagebas" src="dommagebas.png">
-    
-    `;
-    const finclose = document.querySelector("#finclose");
-    const final = "<img src='win.png'>";
+    <img class="dommagebas" src="../minijeu/assets/img/dommagebas.png">`;
+    const finclose = document.querySelector("#finclose")
+    const final = "<img src='../minijeu/assets/img/win.png'>"
     if (score > 1) {
-        console.log("finaleeeee");
-
-        document.querySelector("#gagne").innerHTML = final;
+        document.querySelector("#gagne").innerHTML = final
     } else {
-        document.querySelector("#gagne").innerHTML = looser;
+        document.querySelector("#gagne").innerHTML = looser
     }
-    document.querySelector(".fnpartie").style.display = "block";
+    document.querySelector(".fnpartie").style.display = "block"
     finclose.addEventListener("click", function () {
-        document.querySelector(".fnpartie").style.display = "none";
-    });
+        document.querySelector(".fnpartie").style.display = "none"
+    })
 }
 
-
-
 function attendreAnime() {
-    
     anime.setAttribute("class", "div9 choixjoueursortie2");
     playercard.setAttribute("class", "div9 choixjoueursortie")
-    anime.innerHTML = '<img src="anime2.gif" alt="">'
-    
-
-    playercard.innerHTML = '<img src="card.png" alt="">'
-
+    anime.innerHTML = '<img src="../minijeu/assets/img/anime2.gif" alt="">'
+    playercard.innerHTML = '<img src="../minijeu/assets/img/card.png" alt="">'
     cardsChoise.setAttribute("display", "block")
-
-
-    cardsChoise.setAttribute("class", "div1 inCard");
-
-
-
+    cardsChoise.setAttribute("class", "div1 inCard")
 }
 
 close.addEventListener("click", function () {
-    document.querySelector(".modal").style.display = "none";
-});
-
+    document.querySelector(".modal").style.display = "none"
+})
